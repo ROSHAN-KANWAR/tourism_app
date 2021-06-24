f3bc7c05c21254387e1d1d712f3e1734e551a5a2
ROSHAN17052 <cpkroshan@gmail.com>
2021-06-24 06:21:26 +0530

    ok

--- newcgtourism/settings.py
+++ newcgtourism/settings.py
@@ -105,8 +105,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
-STATICFILES_DIR = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIR =[
+     os.path.join(BASE_DIR, 'static')
+]
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
